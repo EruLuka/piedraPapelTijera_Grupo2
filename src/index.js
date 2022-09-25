e0c92f062b66703de2566efe53b4fbbbb582cd8c
Conrado Velazquez <29915636@fi.unju.edu.ar>
2022-09-25 01:28:40 -0300

    Conrado: cambios en juego

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Juego from './components/Juego';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Juego />
   </React.StrictMode>
 );
