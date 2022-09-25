13e0f2e2bb030786a0fdb3e5fad3046e96fdf43f
Gryuker <eskeleto@1958.outlook.es>
2022-09-25 01:38:10 -0300

    Agustin: PasarDatos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import Integrantes from "./components/Integrantes";
+import './components/despedida.css';
+//import Integrantes from "./components/Integrantes";
+import Despedida from "./components/despedida";
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <Integrantes/>
+    </div>
+  );
+}
+*/
+function App() {
+  return (
+    <div className="App">
+      <Despedida/>
     </div>
   );
 }
